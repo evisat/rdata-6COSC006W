f6ea7b35fb3f8e95a324b53c045abdc068a39c14
evisat <evisaterziu@googlemail.com>
2019-02-20 23:12:09 +0000

    data cleaning and first try at producing scatterplots with ggplot

--- R/loadData.R
+++ R/loadData.R
@@ -1,5 +1,5 @@
 studentData <- read.csv("Source/uowdata.csv")
-# view the first handful of rows
+# view the first 6 rows
 head(studentData)
 # shows dimensions of data frame. first value shows number of columns and second value shows number of rows
 dim(studentData)
@@ -9,3 +9,119 @@ names(studentData)
 str(studentData)
 #quick summary of each column
 summary(studentData)
+
+#get number of rows that are in the Harrow Campus
+harrow_campus <- subset(studentData, CAMPUSPOSTCODE == "HA1 3TP")
+
+#get number of rows that are in the Cavendish Campus
+cavendish_campus <- subset(studentData, CAMPUSPOSTCODE == "W1W 6XH")
+
+#get number of rows that are in the Regent Campus
+regent_campus <- subset(studentData, CAMPUSPOSTCODE == "W1B 2HW")
+
+#get number of rows that are in the Marylebone Campus
+marylebone_campus <- subset(studentData, CAMPUSPOSTCODE == "NW1 5LS")
+
+#get number of rows that are in Level 4
+level4 <- subset(studentData, STUDYLEVEL == "Level 4")
+
+#get number of rows that are in Level 5
+level5 <- subset(studentData, STUDYLEVEL == "Level 5")
+
+#get number of rows that are in Level 6
+level6 <- subset(studentData, STUDYLEVEL == "Level 6")
+
+#get number of rows that are in 2016/2017
+year1617 <- subset(studentData, YEAR == "2016/17")
+
+#get number of rows that are in 2017/2018
+year1718 <- subset(studentData, YEAR == "2017/18")
+
+#install dplyr package for data transformation packages
+library(dplyr)
+
+#filters through the data ignoring false or NA values
+#na.omit can be used to filter and remove NA values, however the row numbers will remain the same
+#so it will essentially look like rows are missing when they're not actually.
+studentData2 = filter(studentData)
+
+#Exploring and summerising NA in data
+is.na(studentData)
+any(is.na(studentData))
+colSums(is.na(studentData))
+sum(is.na(studentData))
+
+#create a new column to store the number of occurences of NA values in each row
+studentData$na_count <- apply(is.na(studentData), 1, sum)
+sum(studentData$na_count > 1)
+#669 rows with NA values
+#However only two rows that have more then 1 one NA value
+#the command below returns all row indexes and columns that contain NA values
+newdataset <- which(is.na(studentData), arr.ind=TRUE)
+
+#Removing NAs
+#22500 - 669 = 21831
+studentData.clean <- na.omit(studentData) 
+row.names(studentData.clean) <- NULL
+complete.cases(studentData.clean)
+
+with(studentData.clean, plot(COMMUTELENGTH, ACTUALATTENDANCEDAYS/EXPECTEDATTENDANCEDAYS * 100))
+with(studentData.clean, plot(COMMUTELENGTH, AVERAGEMODULEMARK))
+
+summary(studentData.clean$COMMUTELENGTH)
+
+#ONLY Computer Science students 
+compSci <- subset(studentData.clean, NEWCOURSETITLE == "BSc Computer Science FT")
+row.names(compSci) <- NULL
+summary(compSci)
+str(compSci$NEWCOURSETITLE)
+with(compSci, plot(COMMUTELENGTH, ACTUALATTENDANCEDAYS/EXPECTEDATTENDANCEDAYS * 100))
+with(compSci, plot(COMMUTELENGTH, AVERAGEMODULEMARK))
+
+#ONLY Architecture students 
+Architec <- subset(studentData.clean, NEWCOURSETITLE == "BA Architecture FT")
+row.names(Architec) <- NULL
+summary(Architec)
+str(Architec$NEWCOURSETITLE)
+with(Architec, plot(COMMUTELENGTH, ACTUALATTENDANCEDAYS/EXPECTEDATTENDANCEDAYS * 100))
+with(Architec, plot(COMMUTELENGTH, AVERAGEMODULEMARK))
+
+
+#ONLY law students 
+Law <- subset(studentData.clean, NEWCOURSETITLE == "LLB Law FT")
+Law <- subset(studentData.clean, NEWCOURSETITLE == "LLB Law FT" & ACCOMMODATIONTYPE == "UoW Halls")
+Law <- subset(studentData.clean, NEWCOURSETITLE == "LLB Law FT" & ACCOMMODATIONTYPE == "Commuter")
+Law <- subset(studentData.clean, NEWCOURSETITLE == "LLB Law FT" & ACCOMMODATIONTYPE == "Other inc. Private Halls")
+Law <- subset(studentData.clean, NEWCOURSETITLE == "LLB Law FT" & ACCOMMODATIONTYPE == "Unknown")
+
+row.names(Law) <- NULL
+summary(Law)
+str(Law$NEWCOURSETITLE)
+with(Law, plot(COMMUTELENGTH, ACTUALATTENDANCEDAYS/EXPECTEDATTENDANCEDAYS * 100))
+with(Law, plot(COMMUTELENGTH, AVERAGEMODULEMARK))
+
+library(ggplot2)
+
+d <- ggplot(studentData.clean, aes(x=COMMUTELENGTH, y=ACTUALATTENDANCEDAYS/EXPECTEDATTENDANCEDAYS * 100)) +
+  geom_point(shape=1) +    # Use hollow circles
+  geom_smooth(method=lm)   # Add linear regression line 
+#  (by default includes 95% confidence region)
+
+d + geom_point(aes(colour = factor(AVERAGEMODULEMARK)))
+
+# Round xvar and yvar to the nearest 5
+xaxis <- round(Law$COMMUTELENGTH/5)*5
+yaxis <- round((Law$ACTUALATTENDANCEDAYS/Law$EXPECTEDATTENDANCEDAYS * 100)/5)*5
+
+# Make each dot partially transparent, with 1/4 opacity
+# For heavy overplotting, try using smaller values
+ggplot(aes(x=xaxis, y=yaxis)) +
+  geom_point(shape=19,      # Use solid circles
+             alpha=1/4)     # 1/4 opacity
+
+
+# Jitter the points
+# Jitter range is 1 on the x-axis, .5 on the y-axis
+ggplot(dat, aes(x=xrnd, y=yrnd)) +
+  geom_point(shape=1,      # Use hollow circles
+             position=position_jitter(width=1,height=.5))
