91af04037a0d974aec5895fdf7b638aed4df4e0a
evisat <evisaterziu@googlemail.com>
2019-03-03 22:58:22 +0000

    cleaned up codde and some analysis

--- Analysis/07_averagemark.by.attendance.per.course.R
+++ Analysis/07_averagemark.by.attendance.per.course.R
@@ -1,17 +1,34 @@
 glimpse(studentData.clean)
 
+average.attendance.per.course <- studentData.clean %>%
+  group_by(NEWCOURSETITLE) %>%
+  summarise(avg_attendance = mean(PERCENTAGEATTENDANCE)) %>%
+  arrange(desc(avg_attendance))
+
+averagemark.per.course <- studentData.clean %>%
+  group_by(NEWCOURSETITLE) %>%
+  summarise(avg_mark = mean(AVERAGEMODULEMARK)) %>% 
+  arrange(desc(avg_mark))
+
+#Using the top value from the results above to determine limits for graph
+averagemark.by.attendance.per.course <-
+  studentData.clean %>%
+  filter(AVERAGEMODULEMARK <= 70 & PERCENTAGEATTENDANCE <= 70)
+
+averagemark.by.attendance.per.course
+
 #Make plot
 averagemark.by.attendance.per.course.plot <- 
-  ggplot(studentData.clean, aes(x = PERCENTAGEATTENDANCE, y = AVERAGEMODULEMARK)) +
+  ggplot(averagemark.by.attendance.per.course, aes(x = PERCENTAGEATTENDANCE, y = AVERAGEMODULEMARK)) +
   geom_point(size = 1, pch = 21, colour = "black") +
   facet_wrap( ~ NEWCOURSETITLE, nrow = 8) +
   geom_smooth(method=lm) +
-  xlim (-1, 101) +
-  ylim (-5, 101) +
+  xlim (-1, 70) +
+  ylim (-5, 70) +
   labs(title = "A scatterplot showing the relationship between attendance and average module mark",
        subtitle = "")
 
 averagemark.by.attendance.per.course.plot
 
 #save plot as png
-save_plot(averagemark.by.attendance.per.course.plot, 900, 640, "averagemark_by_attendance_per_course.png")
+save_plot(averagemark.by.attendance.per.course.plot, 1000, 640, "averagemark_by_attendance_per_course.png")
--- Analysis/08_averagemark.by.commutelength.per.course.R
+++ Analysis/08_averagemark.by.commutelength.per.course.R
@@ -1,17 +1,34 @@
 glimpse(studentData.clean)
 
+average.commutelengths.per.course <- studentData.clean %>%
+  group_by(NEWCOURSETITLE) %>%
+  summarise(avg_commutelength = mean(COMMUTELENGTH)) %>%
+  arrange(desc(avg_commutelength))
+
+averagemark.per.course <- studentData.clean %>%
+  group_by(NEWCOURSETITLE) %>%
+  summarise(avg_mark = mean(AVERAGEMODULEMARK)) %>% 
+  arrange(desc(avg_mark))
+
+#Using the top value from the results above to determine limits for graph
+averagemark.by.commutelength.per.course <-
+  studentData.clean %>%
+  filter(AVERAGEMODULEMARK <= 70 & COMMUTELENGTH <= 20)
+
+averagemark.by.commutelength.per.course
+
 #Make plot
 averagemark.by.commutelength.per.course.plot <- 
-  ggplot(studentData.clean, aes(x = COMMUTELENGTH, y = AVERAGEMODULEMARK)) +
+  ggplot(averagemark.by.commutelength.per.course, aes(x = COMMUTELENGTH, y = AVERAGEMODULEMARK)) +
   geom_point(size = 1, pch = 21, colour = "black") +
   facet_wrap( ~ NEWCOURSETITLE, nrow = 8) +
   geom_smooth(method=lm) +
-  xlim (-1, 101) +
-  ylim (-5, 101) +
+  xlim (-1, 20) +
+  ylim (-5, 70) +
   labs(title = "A scatterplot showing the relationship between commute length and average module mark",
        subtitle = "")
 
 averagemark.by.commutelength.per.course.plot
 
 #save plot as png
-save_plot(averagemark.by.commutelength.per.course.plot, 800, 640, "averagemark_by_commutelength_per_course.png")
+save_plot(averagemark.by.commutelength.per.course.plot, 1000, 640, "averagemark_by_commutelength_per_course.png")
