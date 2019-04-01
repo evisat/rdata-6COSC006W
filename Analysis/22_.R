3afb73f7c1ad58125a1be56ddb77c8bd16486042
evisat <evisaterziu@googlemail.com>
2019-04-01 21:15:08 +0100

    created pie charts to show different degree classifications for each degree type

--- Analysis/22_.R
+++ Analysis/22_.R
@@ -1,54 +1,37 @@
-total.averagemarks.by.classification.per.degreetype <- studentData.clean %>%
-  filter((STUDYLEVEL == 'Level 6') & (AVERAGEMODULEMARK >= 40)) %>%
-  group_by(DEGREETYPE) %>%
-  summarize('1st' = sum(AVERAGEMODULEMARK >= 70),
-            '2:i' = sum(AVERAGEMODULEMARK >= 60 & AVERAGEMODULEMARK < 70),
-            '2:ii' = sum(AVERAGEMODULEMARK >= 50 & AVERAGEMODULEMARK < 60),
-            '3rd' = sum(AVERAGEMODULEMARK < 50))
-
-total.averagemarks.by.classification.per.degreetype <- t(total.averagemarks.by.classification.per.degreetype)
-
-
-##### BA
-totalBA <- total_averagemarks_by_classification_per_degreetype %>%
-  filter(DEGREETYPE == 'BA')
-
-# reverse the order of the columns and rows
-reverseBA <- data.frame(t(totalBA[-1]))
-colnames(reverseBA) <- totalBA[, 1]
-
-#Make plot
-blank_theme <- theme_minimal()+
-  theme(
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
-    legend.title = element_blank(),
-    panel.border = element_blank(),
-    panel.grid=element_blank(),
-    axis.ticks = element_blank(),
-    axis.text = element_blank(),
-    plot.title=element_text(size=14, face="bold")
-  )
-
 library(scales)
 
-pie <-
-  ggplot(reverseBA,
-         aes(x = "", y = BA, fill = rownames(reverseBA))) +
-  coord_polar(theta = "y") +
-  theme(axis.text.x=element_blank()) +
-  blank_theme +
-  geom_bar(stat="identity",
-           width = 1)
+#add column to represent the degree classification e.g. 1st, 2:i, 2:ii, 3rd - ignoring marks under 40 
+studentData.clean.class <- studentData.clean %>%
+  filter((AVERAGEMODULEMARK >= 40)) %>%
+  mutate(DEGREECLASS = ifelse(AVERAGEMODULEMARK >= 70, '1st', 
+                              ifelse(AVERAGEMODULEMARK >= 60 & AVERAGEMODULEMARK < 70, '2:i',
+                                     ifelse(AVERAGEMODULEMARK >= 50 & AVERAGEMODULEMARK < 60, '2:ii', '3rd'))))
 
-pie
+pie.plot.data <- studentData.clean.class %>%
+  group_by(DEGREETYPE, DEGREECLASS) %>%
+  summarize(COUNT = sum(AVERAGEMODULEMARK >= 40)) %>%
+  arrange(desc(DEGREECLASS)) 
 
-pie + scale_fill_brewer("Blues") + blank_theme +
-  theme(axis.text.x=element_blank())+
-  geom_text(aes(y = value/3 + c(0, cumsum(value)[-length(value)]), 
-                label =  percent(value/100)), size=5)
+pie.plot.data <- pie.plot.data %>%
+  mutate(perc = round(COUNT/sum(pie.plot.data[which(pie.plot.data$DEGREETYPE == DEGREETYPE),"COUNT"]) * 100, digits = 2), lab.ypos = round(cumsum(perc) - 0.5*perc, digits = 2))
 
+mycols <- c("#EDAE49", "#D1495B", "#00798C", "#424B54")
 
+pie.plot <-
+  ggplot(pie.plot.data, aes(x = 2, y = perc, fill = DEGREECLASS)) +
+  geom_bar(stat = "identity", color = "white") +
+  coord_polar(theta = "y", start = 0) +
+  geom_text(aes(y = lab.ypos, label = paste(perc, "%", sep="")), color = "white") +
+  scale_fill_manual(values = mycols, name="Degree Classification") +
+  ggtitle("Average Classifications of students at the University of Westminster per Degree Type") +
+  theme_void() +
+  theme( plot.title = element_text(size = 27, margin = margin(t = 0, r = 0, b = 30, l = 0, unit = "pt")),
+         legend.title = element_text(size = 20),
+         legend.text = element_text(size = 16, margin = margin(t = 0, r = 0, b = 5, l = 0, unit = "pt")),
+         strip.text = element_text(size = 20, margin = margin(b=5)),
+         plot.margin=unit(c(1,1,1.5,1.2),"cm")) +
+  facet_wrap(~ DEGREETYPE) +
+  xlim(0.5, 2.5)
 
 #save plot as png
-save_plot(averagemark.per.year.by.course.plot, 700, 840, "averagemark_per_year_per_course_Harrow.png")
+save_plot(pie.plot, 1500, 940, "avg_class_per_degreeType_pie_chart.png")
